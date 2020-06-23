344fea04b98d9dfad950a92634c74272bd61c070
Jing Wang <278161009@qq.com>
2020-06-23 21:19:29 +0800

    update rnd

---
+++ hm_shop/pages/contact/contact.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="contact">
+		<image class="img" src="http://www.itcast.cn/2018czydz/images/gywmban.jpg"></image>
+		<view class="info">
+			<view @click="phone">联系电话：18928357301(点击拨打电话)</view>
+			<view>联系地址: 惠州市惠城区</view>
+		</view>
+		<map class="map" :longitude="longitude" :scale="scale" :latitude="latitude" :markers="markers"></map>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				longitude:114.418978,
+				latitude:23.138876,
+				scale:13,
+				markers:[
+					{
+						longitude:114.418978,
+						latitude:23.138876,
+						iconPath:'../../static/hmlogo.png',
+						width:30,
+						height:30
+					}
+				]
+			}
+		},
+		methods: {
+			phone(){
+				uni.makePhoneCall({
+					phoneNumber:'18928357301'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.contact{
+		.img{
+			width: 750rpx;
+			height: 320rpx;
+		}
+		
+		.info{
+			padding: 10rpx 20rpx;
+			font-size: 30rpx;
+			view{
+				line-height: 80rpx;
+				border-bottom: 1px #eee solid;
+			}
+			
+		}
+		
+		.map{
+			width: 750rpx;
+			height: 750rpx;
+		}
+		
+	}
+</style>
---
+++ hm_shop/pages/pics/pics.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="pics">
+		<scroll-view class="left" scroll-y>
+			<view class="active">家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+			<view>家居生活</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	page{
+		height: 100%;
+	}
+	.pics{
+		height: 100%;
+		.left{
+			width: 200rpx;
+			height: 100%;
+			border-right: 1px solid #eee;
+			view{
+				height: 60px;
+				line-height: 60px;
+				color: #333;
+				text-align: center;
+				font-size: 30rpx;
+				border-top: 1px solid #eee;
+			}
+			.active{
+				background: $shop-color;
+				color: #FFF;
+			}
+		}
+		
+	}
+</style>
